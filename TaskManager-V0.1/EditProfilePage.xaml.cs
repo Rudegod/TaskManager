4ad80181cabd5a3955683a6e4b0e0b65cb036859
ErfanH2006 <erfanhabibi258888@gmail.com>
2024-09-22 01:16:06 +0330

    EditProfilePage is complet by NOW .

--- TaskManager-V0.1/EditProfilePage.xaml.cs
+++ TaskManager-V0.1/EditProfilePage.xaml.cs
@@ -1,11 +1,14 @@
+using Microsoft.Maui.Storage;
+using System.IO.Enumeration;
+
 namespace TaskManager_V0._1;
 
 public partial class EditProfilePage : ContentPage
 {
-	public EditProfilePage()
-	{
-		InitializeComponent();
-	}
+    public EditProfilePage()
+    {
+        InitializeComponent();
+    }
     private async void OnSaveChangesClicked(object sender, EventArgs e)
     {
         // Here you would add logic to save the user data
@@ -18,7 +21,46 @@ public partial class EditProfilePage : ContentPage
         bool confirm = await DisplayAlert("Confirm", "Are you sure you want to cancel?", "Yes", "No");
         if (confirm)
         {
-            await Navigation.PopAsync();
+            await Navigation.PopModalAsync();
+        }
+    }
+
+    private async void OnChangeProfileClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            var fileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>()
+            {
+                { DevicePlatform.iOS, new[] { "public.image" } }, // iOS image types
+                { DevicePlatform.Android, new[] { "image/*" } }, // Android image types
+                { DevicePlatform.WinUI, new[] { ".jpg", ".jpeg", ".png", ".gif", ".bmp" } } // Windows image types
+            });
+
+            // Open the file picker with specified file types
+            var result = await FilePicker.PickAsync(new PickOptions
+            {
+                PickerTitle = "Pick an Image",
+                FileTypes = fileTypes
+            });
+
+            if (result != null)
+            {
+                // Get the file's path
+                var filePath = result.FullPath;
+                // You can also access the file's stream if needed
+                using (var stream = await result.OpenReadAsync())
+                {
+                    // Process the file stream as needed
+                }
+
+                // Update the label to show the picked file
+                FileLabel.Text = $"Picked file: {result.FileName}";
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle any exceptions, such as user cancellation
+            FileLabel.Text = $"Error picking file: {ex.Message}";
         }
     }
 }
