975d8ebe61377da23a59eaac4a3ab8e909887784
mahdieh <mahdiehmokht@gamil.com>
2024-09-22 11:10:42 +0330

    another one

--- TaskManager-V0.1/PeoplePage.xaml.cs
+++ TaskManager-V0.1/PeoplePage.xaml.cs
@@ -1,3 +1,6 @@
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Views;
+
 namespace TaskManager_V0._1;
 
 public partial class PeoplePage : ContentPage
@@ -5,6 +8,8 @@ public partial class PeoplePage : ContentPage
 	public PeoplePage()
 	{
 		InitializeComponent();
+        membersList.ItemsSource = MemberData.members;
+        //BindingContext = this;
         membersList.ItemsSource = TempData.members;
         TempData.members.Add(new Member());
         TempData.members.Add(new Member());
@@ -19,5 +24,19 @@ public partial class PeoplePage : ContentPage
     private void TapGestureRecognizer_Tapped_1(object sender, TappedEventArgs e)
     {
         DisplayAlert("here", "It works", "ahaaa");
+    }
+
+    private void addButton(object sender, EventArgs e)
+    {
+        var popup = new addMemberPopup();
+        this.ShowPopup(popup);
+        popup.Closed += addMember;
+    }
+    public void addMember(object sender, PopupClosedEventArgs e)
+    {
+        if (Member.f)
+            MemberData.members.Add(new Member(MemberData.fullName, MemberData.userName, MemberData.password, MemberData.age,
+                MemberData.gender, MemberData.phone, MemberData.nationalCode, MemberData.active));
+        Member.f = false;
     }
 }
