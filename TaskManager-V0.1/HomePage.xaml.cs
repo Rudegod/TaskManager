05011a735d83e8c8fd9b29a223db7ebb04e4d12d
mahdieh <mahdiehmokht@gamil.com>
2024-09-26 12:03:41 +0330

    check

--- TaskManager-V0.1/HomePage.xaml.cs
+++ TaskManager-V0.1/HomePage.xaml.cs
@@ -5,12 +5,9 @@ namespace TaskManager_V0._1;
 
 public partial class HomePage : ContentPage
 {
-    private bool _isMenuOpen = false;
     public HomePage()
 	{
         InitializeComponent();
-        // Set initial position of the menu off-screen
-        OnHamburger.TranslationX = -OnHamburger.Width;
     }
 
     private void peopleButton(object sender, EventArgs e)
@@ -30,38 +27,5 @@ public partial class HomePage : ContentPage
     private void ProfileButton(object sender, EventArgs e)
     {
         Navigation.PushModalAsync(new ProfiePage());
-    }
-
-    private async void OnHamburgerMenuClicked(object sender, EventArgs e)
-    {
-        if (_isMenuOpen)
-        {
-            await CloseMenu();
-        }
-        else
-        {
-            await OpenMenu();
-        }
-    }
-
-    private async Task OpenMenu()
-    {
-        OnHamburger.IsVisible = true;
-        await OnHamburger.TranslateTo(0, 0, 300, Easing.CubicInOut);
-        _isMenuOpen = true;
-    }
-
-    private async Task CloseMenu()
-    {
-        await OnHamburger.TranslateTo(-OnHamburger.Width, 0, 300, Easing.CubicInOut);
-        _isMenuOpen = false;
-    }
-
-    private async void OnMainAreaTapped(object sender, TappedEventArgs e)
-    {
-        if (_isMenuOpen)
-        {
-            await CloseMenu();
-        }
     }
 }
