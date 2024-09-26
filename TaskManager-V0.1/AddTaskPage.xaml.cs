ded0699849a29766b75f434b11464af9f4a1b40a
mahdieh <mahdiehmokht@gamil.com>
2024-09-26 11:55:39 +0330

    unsure this code works

--- TaskManager-V0.1/AddTaskPage.xaml.cs
+++ TaskManager-V0.1/AddTaskPage.xaml.cs
@@ -10,64 +10,13 @@ public partial class AddTaskPage : ContentPage
         InitializeComponent();
     }
 
-    private void PreShowButton(object sender, EventArgs e)
-    {
-        Popup.IsVisible = true;
-    }
-
-    private void PreCloseButton(object sender, EventArgs e)
-    {
-        Popup.IsVisible = false;
-    }
-
-    private void CouShowButton(object sender, EventArgs e)
-    {
-        Popup2.IsVisible = true;
-    }
-
-    private void CouCloseButton(object sender, EventArgs e)
-    {
-        Popup2.IsVisible = false;
-    }
-
-    private void ManagerShowButton(object sender, EventArgs e)
-    {
-        Popup3.IsVisible = true;
-    }
-
-    private void ManagerCloseButton(object sender, EventArgs e)
-    {
-        Popup3.IsVisible = false;
-    }
-
-    private void PreviousShowButton(object sender, EventArgs e)
-    {
-        Popup4.IsVisible = true;
-    }
-
-    private void PreviousCloseButton(object sender, EventArgs e)
-    {
-        Popup4.IsVisible = false;
-    }
-
-    private void OKButton(object sender, EventArgs e)
-    {
-        // Navigation.PushAsync(new TaskPage());
-        App.Current.MainPage = new TaskPage();
-    }
-
-    private void AssignShowButton(object sender, EventArgs e)
-    {
-        Popup6.IsVisible = true;
-    }
-
-    private void AssignCloseButton(object sender, EventArgs e)
-    {
-        Popup6.IsVisible = false;
-    }
-
     private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
     {
         importanceValueLabel.Text = ((int)e.NewValue).ToString();
+    }
+
+    private void okButton(object sender, EventArgs e)
+    {
+
     }
 }
